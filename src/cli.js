0d5d8195647d974c9424cc42ead11e17c811d006
Nate Murray <nate@natemurray.com>
2016-06-17 16:07:57 -0700

    show help if no query passed

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,18 @@ import yargs from 'yargs';
 import fs from 'fs';
 
 let argv = yargs
-    .usage('Usage: $0 <command> [options]')
-    .example("$0 '.foo .bar'", "show foo bar")
+    .usage('Usage: $0 <query> <file>')
+    .example("$0 '.MyClass .fooFunction'", "show code for fooFunction() in MyClass")
     .help('h')
     .alias('h', 'help')
     .argv;
 
 let [query, filename] = argv._;
+
+if(!query) {
+  yargs.showHelp();
+  process.exit();
+}
 
 let inputStream = filename ? fs.createReadStream(filename) : process.stdin;
 

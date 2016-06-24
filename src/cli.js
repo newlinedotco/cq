c1c7a8d0c391adaa02621f6e738428c7ff3c4464
Nate Murray <nate@natemurray.com>
2016-06-24 13:54:00 -0700

    added engine selection in the cli

--- src/cli.js
+++ src/cli.js
@@ -13,6 +13,17 @@ let argv = yargs
       type: 'boolean',
       describe: 'Output results in machine-readable format'
     })
+    .option('short', {
+      alias: 's',
+      type: 'boolean',
+      describe: 'emit short json result (no code). requires --json'
+    })
+    .option('engine', {
+      alias: 'e',
+      describe: 'parsing engine',
+      choices: ['auto', 'babylon', 'typescript'],
+      default: 'auto'
+    })
     .argv;
 
 let [query, filename] = argv._;
@@ -22,16 +33,38 @@ if(!query) {
   process.exit();
 }
 
+let engine;
+
+// pick the parsing engine
+switch (argv.engine) {
+case 'babylon':
+case 'typescript':
+  engine = argv.engine;
+  break;
+case 'auto':
+  if(filename && filename.match(/.tsx?/)) {
+    engine = 'typescript';
+  } else {
+    engine = 'babylon';
+  }
+  break;
+default:
+  throw new Error('unknown engine: ' + argv.engine);
+}
+
 let inputStream = filename ? fs.createReadStream(filename) : process.stdin;
 
 var content = '';
 inputStream.resume();
 inputStream.on('data', function(buf) { content += buf.toString(); });
 inputStream.on('end', function() {
-  let result = cq(content, query);
+  let result = cq(content, query, { engine });
 
   if(argv.json === true) {
     delete result['nodes'];
+    if(argv.short === true) {
+      delete result['code'];
+    }
     console.log( JSON.stringify(result, null, 2) );
   } else {
     console.log( result.code );

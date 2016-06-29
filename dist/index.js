5f4d44e13528307ba590a4df7f64ffae7719e0cd
Nate Murray <nate@natemurray.com>
2016-06-28 19:10:07 -0700

    prepublish

--- dist/index.js
+++ dist/index.js
@@ -279,11 +279,30 @@ function resolveIndividualQuery(ast, root, code, query, engine, opts) {
 
         var args = _query$arguments.slice(1);
 
+        var handled = false;
+        // some operators modify before the target query
+        // e.g. after() actually has two queries
+        // TODO clean this up to unify design with `modifyAnswerWithCall`. `handled` is icky
+        switch (callee) {
+          case 'after':
+            handled = true;
+
+            var _args3 = _slicedToArray(args, 1);
+
+            var goalpostQuery = _args3[0];
+
+            var goalpostNode = resolveIndividualQuery(ast, root, code, goalpostQuery, engine, opts);
+            opts.after = goalpostNode.end;
+            break;
+        }
+
         var answer = resolveIndividualQuery(ast, root, code, childQuery, engine, opts);
 
         // whatever the child answer is, now we modify it given our callee
         // TODO - modifying the asnwer needs to be given not only the answer start and end range, but the child node which returned that start and end
-        answer = modifyAnswerWithCall(ast, code, callee, args, engine, answer);
+        if (!handled) {
+          answer = modifyAnswerWithCall(ast, code, callee, args, engine, answer);
+        }
 
         // hmm, maybe do this later in the pipeline?
         answer.code = code.substring(answer.start, answer.end);

c8e7b91647f5470ce2fa38e86d4756f6228d2ac8
Nate Murray <nate@natemurray.com>
2017-03-16 11:07:21 -0500

    prettier

--- packages/cq/src/engines/util.js
+++ packages/cq/src/engines/util.js
@@ -8,13 +8,13 @@
 function rangeExtents(ranges) {
   let start = Number.MAX_VALUE;
   let end = Number.MIN_VALUE;
-  ranges.map(({start: rs, end: re}) => {
+  ranges.map(({ start: rs, end: re }) => {
     start = Math.min(start, rs);
     end = Math.max(end, re);
   });
-  return {start,end};
+  return { start, end };
 }
 
 export default {
   rangeExtents
-}
+};

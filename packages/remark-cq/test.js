deaca32edb3f349a02b864b4facf5313dd053756
Nate Murray <nate@natemurray.com>
2018-12-02 16:42:29 -0600

    indent 2

--- packages/remark-cq/test.js
+++ packages/remark-cq/test.js
@@ -24,90 +24,90 @@ const remark2rehype = require("remark-rehype");
 const remarkStringify = require("remark-stringify");
 
 const render = (text, config) =>
-    unified()
-        .use(reParse)
-        .use(plugin, Object.assign({ root: __dirname }, config))
-        .use(remark2rehype)
-        .use(stringify)
-        .process(text);
+  unified()
+    .use(reParse)
+    .use(plugin, Object.assign({ root: __dirname }, config))
+    .use(remark2rehype)
+    .use(stringify)
+    .process(text);
 
 const renderMarkdown = (text, config) =>
-    unified()
-        .use(reParse)
-        .use(remarkStringify)
-        .use(plugin, config)
-        .process(text);
+  unified()
+    .use(reParse)
+    .use(remarkStringify)
+    .use(plugin, config)
+    .process(text);
 
 /*
  * Tests.
  */
 
 test("remark-cq code imports crop-query works", async t => {
-    const dogs = () => "Like snuggles";
-    const markup = `
+  const dogs = () => "Like snuggles";
+  const markup = `
 The code:
 
 {lang=javascript,crop-query=.dogs}  
 <<[](test.js)`;
-    const actual = (await render(markup, { root: __dirname })).contents;
-    const expected = `<p>The code:</p>
+  const actual = (await render(markup, { root: __dirname })).contents;
+  const expected = `<p>The code:</p>
 <pre><code class="language-javascript">const dogs = () => "Like snuggles";
 </code></pre>`;
-    t.equal(actual, expected);
-    t.end();
+  t.equal(actual, expected);
+  t.end();
 });
 
 test("remark-cq code imports line numbers works", async t => {
-    const markup = `
+  const markup = `
 The code:
 
 {lang=javascript,crop-start-line=1,crop-end-line=2}
 <<[](test.js)`;
-    const actual = (await render(markup, { root: __dirname })).contents;
-    const expected = `<p>The code:</p>
+  const actual = (await render(markup, { root: __dirname })).contents;
+  const expected = `<p>The code:</p>
 <pre><code class="language-javascript">/**
  * @author Nate Murray
 </code></pre>`;
-    t.equal(actual, expected);
-    t.end();
+  t.equal(actual, expected);
+  t.end();
 });
 
 test("remark-cq code doesn't break normal blocks", async t => {
-    const markup = `
+  const markup = `
 The code:
 
 \`\`\`javascript
 var foo = 1;
 \`\`\``;
-    const actual = (await render(markup, { root: __dirname })).contents;
-    const expected = `<p>The code:</p>
+  const actual = (await render(markup, { root: __dirname })).contents;
+  const expected = `<p>The code:</p>
 <pre><code class="language-javascript">var foo = 1;
 </code></pre>`;
-    t.equal(actual, expected);
-    t.end();
+  t.equal(actual, expected);
+  t.end();
 });
 
 test("remark-cq code imports compile back to markdown", async t => {
-    const dogs = () => "Like snuggles";
-    const markup = `
+  const dogs = () => "Like snuggles";
+  const markup = `
 The code:
 
 {lang=javascript,crop-query=.dogs}
 <<[](test.js)`;
-    const actual = (await renderMarkdown(markup, { root: __dirname })).contents;
-    const expected = `The code:
+  const actual = (await renderMarkdown(markup, { root: __dirname })).contents;
+  const expected = `The code:
 
 \`\`\`javascript
 const dogs = () => "Like snuggles";
 \`\`\`
 `;
-    t.equal(actual, expected);
-    t.end();
+  t.equal(actual, expected);
+  t.end();
 });
 
 test("remark-cq code imports compile back to markdown in th emiddle of a document", async t => {
-    const dogs = () => "Like snuggles";
-    const markup = `
+  const dogs = () => "Like snuggles";
+  const markup = `
 The code:
 
 {lang=javascript,crop-query=.dogs}
@@ -120,8 +120,8 @@ and here's the **next** one:
 
 see?
 `;
-    const actual = (await renderMarkdown(markup, { root: __dirname })).contents;
-    const expected = `The code:
+  const actual = (await renderMarkdown(markup, { root: __dirname })).contents;
+  const expected = `The code:
 
 \`\`\`javascript
 const dogs = () => "Like snuggles";
@@ -135,38 +135,38 @@ const dogs = () => "Like snuggles";
 
 see?
 `;
-    t.equal(actual, expected);
-    t.end();
+  t.equal(actual, expected);
+  t.end();
 });
 
 test("remark-cq meta doesn't affect html", async t => {
-    const markup = `
+  const markup = `
 The code:
 
 {lang=javascript,crop-query=.dogs,meta=true}  
 <<[](test.js)`;
-    const actual = (await render(markup, { root: __dirname })).contents;
-    const expected = `<p>The code:</p>
+  const actual = (await render(markup, { root: __dirname })).contents;
+  const expected = `<p>The code:</p>
 <pre><code class="language-javascript">const dogs = () => "Like snuggles";
 </code></pre>`;
-    t.equal(actual, expected);
-    t.end();
+  t.equal(actual, expected);
+  t.end();
 });
 
 test("remark-cq code meta compiles back to markdown", async t => {
-    const dogs = () => "Like snuggles";
-    const markup = `
+  const dogs = () => "Like snuggles";
+  const markup = `
 The code:
 
 {lang=javascript,crop-query=.dogs,meta=all}
 <<[](test.js)`;
-    const actual = (await renderMarkdown(markup, { root: __dirname })).contents;
-    const expected = `The code:
+  const actual = (await renderMarkdown(markup, { root: __dirname })).contents;
+  const expected = `The code:
 
 \`\`\`javascript { actualFilename=test.js endChar=999 endLine=46 startChar=960 startLine=46 }
 const dogs = () => "Like snuggles";
 \`\`\`
 `;
-    t.equal(actual, expected);
-    t.end();
+  t.equal(actual, expected);
+  t.end();
 });
